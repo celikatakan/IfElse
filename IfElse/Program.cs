720fc3790dadf0465b7e5b15b787cc06012f4c47
auro <auro@DESKTOP-AV8I9BT>
2024-07-20 22:02:47 +0300

    Add project files.

---
+++ IfElse/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class IfElse
+
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Lütfen bir sayı giriniz: ");
+
+        int sayı = Convert.ToInt32(Console.ReadLine());
+
+        if (sayı > 10)
+        {
+            Console.WriteLine("Girilen sayı 10'dan büyüktür.");
+
+        }
+        else if (sayı < 10)
+        {
+            Console.WriteLine("Girilen sayı 10'dan küçüktür.");
+        }
+        else
+        {
+            Console.WriteLine("Girilen sayı 10'a eşittir.");
+        }
+
+        if (sayı % 2 == 0)
+        {
+            Console.WriteLine("Girilen sayı çifttir.");
+
+        }
+        else
+        {
+            Console.WriteLine("Girilen sayı tektir.");
+        }
+
+
+
+
+    }
+}
